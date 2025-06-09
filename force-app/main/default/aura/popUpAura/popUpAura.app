11df09919acb2520f628289e2c9f4d72c2cf7389
Mattia Golino <mgolino@minsait.com>
2025-06-09 15:49:55 +0200

    modified popUp and dettagliCase page

---
+++ force-app/main/default/aura/popUpAura/popUpAura.app
@@ -0,0 +1,3 @@
+<aura:application extends="ltng:outApp"     implements="ltng:allowGuestAccess">
+    <c:parentComponent></c:parentComponent>
+</aura:application>
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,10 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import LightningAlert from 'lightning/alert';
 
 export default class ParentComponent extends LightningElement {
-    showModal = false;
+
+    @api recordId;
+    @api stato;
+
+    get isCasoChiuso(){
+        return this.stato === 'Chiuso';
+    }
 
     handleOpen() {
         //this.showModal = true;
         this.refs.popup.openModal();
     }
+
+    handleSuccess() {
+        this.refs.popup.closeModal();
+    }
+
+    salva(){
+        if(this.fields != null){
+            this.template.querySelector('lightning-record-edit-form').submit(this.fields);
+        }else{
+            LightningAlert.open({
+            message: 'Inserisci Risposta',
+            theme: 'error', // a red theme intended for error states
+            label: 'Error!', // this is the header text
+        });
+        }
+    }
+
 }
